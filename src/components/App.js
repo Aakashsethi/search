b0a55922eaa50a0284302c0293e919063c826b71
Aakash Sethi <as3793@njit.edu>
2019-07-09 13:48:41 -0700

    linking App and searchbar to index.js file

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () =>{
+    return (
+    <div> 
+        <form>
+            <input type="text"/>
+        </form>
+    </div>
+    );
+};
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.component{
+    render(){
+        return <div>Search Bar</div>
+    }
+}
+
+export default SearchBar;
