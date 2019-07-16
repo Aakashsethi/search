3e6ab2dc58ccf23ba88847e8203b7f668da48a40
Aakash Sethi <as3793@njit.edu>
2019-07-16 16:28:23 -0400

    bug fix i/p is entered by ES2015 arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-    onInputChange(event){
-        console.log(event.target.value)
+    state ={ term:''}
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term);
     }
     render(){
         return (
         <div className ="ui segment">
-            <form className="ui form">
+            <form onSubmit={(event)=>this.onFormSubmit(event)} className="ui form">
                 <div className="field">
                 <label>Image Search</label>
-                <input type="text" onChange={this.onInputChange}/>
+                <input 
+                type="text" 
+                value={this.state.term} 
+                 onChange={e=>this.setState({term: e.target.value})}/>
                 </div>
             </form>
              
