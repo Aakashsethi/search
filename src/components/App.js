fcb102a2bd8f90f598de67d17805a6b19f727bde
Aakash Sethi <as3793@njit.edu>
2019-07-21 22:09:05 -0400

    created an API call request by axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-class App extends React.Component {
+
+class App extends React.Component{
     onSearchSubmit(term){
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos',{
+            params:{query: term},
+            headers:{
+                Authorization: 'Client-ID c2d6f4b4633c5bc33b12f3d5f93a18c8f97eb9be05bdfcedb04ac7389b1626a8'
+            }
+
+        });
     }
  render(){
     return (
